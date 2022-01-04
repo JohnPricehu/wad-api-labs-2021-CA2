2c55ab78ccfc6e855b1bfa3380c9700ec9a95742
Yiming Hu <20092158@mail.wit.ie>
2022-01-04 23:54:02 +0000

    update movie reviews route

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { movies, movieReviews, movieDetails } from './moviesData';
+// import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import {
-    getUpcomingMovies, getNowPlayingMovies, getTopRatedMovies, getMovieSimilar,getMovieCredits,getMovieImages
+    getUpcomingMovies, getNowPlayingMovies, getTopRatedMovies, getMovieSimilar,getMovieCredits,getMovieImages,getMovieReviews
   } from '../tmdb-api';
 
 const router = express.Router(); 
@@ -43,6 +43,16 @@ router.get('/:id/credits', asyncHandler(async (req, res) => {
     }
 }));
 
+router.get('/:id/reviews', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const reviews = await getMovieReviews(id);
+    if (reviews) {
+        res.status(200).json(reviews);
+    } else {
+        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
+    }
+}));
+
 router.get('/:id/similar', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
     const similarMovie = await getMovieSimilar(id);
@@ -63,27 +73,24 @@ router.get('/:id/images', asyncHandler(async (req, res) => {
     }
 }));
 
-router.get('/:id/reviews', (req, res) => {
+router.get('/:id/reviews', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
-    // find reviews in list
-    if (movieReviews.id == id) {
-        res.status(200).json(movieReviews);
+    const images = await getMovieImages(id);
+    if (images) {
+        res.status(200).json(images);
     } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
+        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
     }
-});
+}));
 
-router.post('/:id/reviews', (req, res) => {
+router.post('/:id/reviews', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
-
-    if (movieReviews.id == id) {
+    const reviews = await getMovieReviews(id);
+    if (reviews.id == id) {
         req.body.created_at = new Date();
         req.body.updated_at = new Date();
         req.body.id = uniqid();
-        movieReviews.results.push(req.body); //push the new review onto the list
+        reviews.results.push(req.body); //push the new review onto the list
         res.status(201).json(req.body);
     } else {
         res.status(404).json({
@@ -91,7 +98,7 @@ router.post('/:id/reviews', (req, res) => {
             status_code: 404
         });
     }
-});
+}));
 
 router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
     const upcomingMovies = await getUpcomingMovies();
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -81,3 +81,19 @@ export const getMovieImages = (id) => {
       throw error
    });
   };
+
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+      ).then( (response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+    
+      })
+      .catch((error) => {
+        throw error
+     });
+    };
+  
