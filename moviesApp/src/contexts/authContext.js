7d2844bdbe7db56e6f3fa71ad2940b8798387181
Yiming Hu <20092158@mail.wit.ie>
2022-01-05 01:23:43 +0000

    update authContext file

--- moviesApp/src/contexts/authContext.js
+++ moviesApp/src/contexts/authContext.js
@@ -1,64 +1,52 @@
-import React , {useContext,useState, useEffect}from 'react'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import React, { useState, createContext } from "react";
+import { login, signup } from "./api/movie-api";
 
+export const AuthContext = createContext(null);
 
-const AuthContext = React.createContext();
-const auth=getAuth()
-export function useAuth(){
-    return useContext(AuthContext);
-}
+const AuthContextProvider = (props) => {
+  const existingToken = localStorage.getItem("token");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authToken, setAuthToken] = useState(existingToken);
+  const [userName, setUserName] = useState("");
 
-export default function AuthProvider({children}) {
-    const [currentUser,setCurrentUser] = useState()
-    const [loading,setLoading] = useState(false)
+  //Function to put JWT token in local storage.
+  const setToken = (data) => {
+    localStorage.setItem("token", data);
+    setAuthToken(data);
+  }
 
-    function signUp(email,password){
-        return createUserWithEmailAndPassword(auth,email,password).then((userCredential) => {
-
-            // const user = userCredential.user;
-
-          })
-          .catch((error) => {
-            // const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage)
-
-          });
+  const authenticate = async (username, password) => {
+    const result = await login(username, password);
+    if (result.token) {
+      setToken(result.token)
+      setIsAuthenticated(true);
+      setUserName(username);
     }
+  };
 
-    function logIn(email,password){
+  const register = async (username, password) => {
+    const result = await signup(username, password);
+    console.log(result.code);
+    return (result.code == 201) ? true : false;
+  };
 
+  const signout = () => {
+    setTimeout(() => setIsAuthenticated(false), 100);
+  }
 
-        return signInWithEmailAndPassword(auth,email,password).then((userCredential) => {
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        authenticate,
+        register,
+        signout,
+        userName
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
-            // const user = userCredential.user;
-          })
-          .catch((error) => {
-            // const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage)
-          });
-    }
-
-    useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-
-        setCurrentUser(user)
-        setLoading(false)
-
-    })
-
-    return unsubscribe
-},[])
-
-    const value = {
-        currentUser,
-        signUp,
-        logIn
-    }
-    return (
-        <AuthContext.Provider value = {value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    )
-} 
+export default AuthContextProvider;
